58dae40207c3131fa43ecbb76d005a20da41771e
Shivam Kaushik <shivam.kaushik73@gmail.com>
2018-12-13 20:25:00 +0530

    Webpack Initial COmmit

---
+++ app/index.js
@@ -0,0 +1,10 @@
+/**
+ * Application entry point
+ */
+
+// Load application styles
+import 'styles/index.sass';
+import 'bootstrap';
+// ================================
+// START YOUR APP HERE
+// ================================
---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// Is the current build a development build
+const IS_DEV = process.env.NODE_ENV === 'dev';
+
+const dirNode = 'node_modules';
+const dirApp = path.join(__dirname, 'app');
+const dirAssets = path.join(__dirname, 'assets');
+
+const appHtmlTitle = 'Webpack Boilerplate';
+
+/**
+ * Webpack Configuration
+ */
+module.exports = {
+  entry: {
+    vendor: ['lodash'],
+    bundle: path.join(dirApp, 'index'),
+  },
+  resolve: {
+    modules: [dirNode, dirApp, dirAssets],
+  },
+  plugins: [
+    new webpack.DefinePlugin({
+      IS_DEV,
+    }),
+
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'index.ejs'),
+      title: appHtmlTitle,
+    }),
+  ],
+  module: {
+    rules: [
+      // BABEL
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /(node_modules)/,
+        options: {
+          compact: true,
+        },
+      },
+
+      // STYLES
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: IS_DEV,
+            },
+          },
+        ],
+      },
+
+      // CSS / SASS
+      {
+        test: /\.(sc|sa)ss/,
+        use: [
+          {
+            loader: 'style-loader', // inject CSS to page
+          },
+          {
+            loader: 'css-loader', // translates CSS into CommonJS modules
+          },
+          {
+            loader: 'postcss-loader', // Run post css actions
+            options: {
+              plugins() {
+                // post css plugins, can be exported to postcss.config.js
+                // eslint-disable-next-line global-require
+                return [require('precss'), require('autoprefixer')];
+              },
+            },
+          },
+          {
+            loader: 'sass-loader', // compiles Sass to CSS
+          },
+        ],
+      },
+
+      // IMAGES
+      {
+        test: /\.(jpe?g|png|gif)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[path][name].[ext]',
+        },
+      },
+    ],
+  },
+};
