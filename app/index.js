63f7daa1a4c2c9b2378c2502a395b76489bc1476
Shivam Kaushik <shivam.kaushik73@gmail.com>
2018-12-14 21:17:47 +0530

    Added Scroll Spy

--- app/index.js
+++ app/index.js
@@ -1,7 +1,8 @@
+/* eslint-disable prefer-destructuring */
+/* eslint-disable func-names */
 /**
  * Application entry point
  */
-
 // Load application styles
 import '../assets/styles/index.sass';
 import 'bootstrap';
@@ -21,3 +22,38 @@ import '../assets/images/services_illus.svg';
 import '../assets/images/contact_bg.svg';
 import '../assets/images/contact_illus_1.svg';
 import '../assets/images/contact_illus_2.svg';
+
+const jquery = require('jquery');
+
+// eslint-disable-next-line no-undef
+window.$ = jquery;
+
+// eslint-disable-next-line no-undef
+$(document).ready(() => {
+  // Add scrollspy to <body>
+  $('body').scrollspy({ target: '.navbar', offset: 50 });
+
+  // Add smooth scrolling on all links inside the navbar
+  $('#navbar-top .navbar-nav .nav-item .nav-link').on('click', function (event) {
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      const hash = this.hash;
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top,
+        },
+        800,
+        () => {
+          // Add hash (#) to URL when done scrolling (default click behavior)
+          window.location.hash = hash;
+        },
+      );
+    } // End if
+  });
+});
